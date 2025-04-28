33bb5ddd42c9a28995c2be155b9f45294f1f2ecf
Gitae-Kwon <kitae0601@gmail.com>
2025-04-29 08:54:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,13 @@ if st.button("🟢 매핑 실행"):
     cols[idx:idx] = ["동일_매핑콘텐츠명", "동일_콘텐츠ID"]
     ordered = front + [c for c in cols if c not in front]
     result  = result[ordered]
-    )
   
     result.drop(
     columns=["동일_콘텐츠ID", "최종_정렬된_매핑되지않은_상품명", "최종_매핑되지않은_상품명"],
     errors="ignore",
     inplace=True,
-
+    )
+  
     # 11) 결과 저장 & 다운로드
     buffer = io.BytesIO()
     with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
